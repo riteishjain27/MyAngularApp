5e0e559449eebc95d42e819adce880a673ff4140
Ritesh Jain <rjai51@sapient.com>
2018-02-28 15:17:35 +0530

    pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() 
+  {
+    // setTimeout(function(){
+    //   $(document).ready(function(){
+    //     $('#example').DataTable( {
+    //       scrollY:        "300px",
+    //       scrollX:        true,
+    //       scrollCollapse: true,
+    //       paging:         false,
+    //       fixedColumns:   {
+    //         leftColumns: 2
+    //     }
+    //   } );
+    // });
+    // }, 1000);
+  
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import {TabComponent} from './tabs/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabComponent
   ],
   imports: [
     BrowserModule
